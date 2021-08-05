191ceb10444336cd20c60c20f90eeed4f82df2eb
levina <82658782+levina-lab@users.noreply.github.com>
2021-08-05 13:41:27 +0700

    small fix

--- totalmsgs.py
+++ totalmsgs.py
@@ -1,7 +1,5 @@
-# credit https://t.me/I_m_FlaSh
-
 """
-✘ Commands Available -
+📚 Commands Available -
 • `{i}totalmsgs`
     Returns your total msg count in current chat
     
@@ -10,6 +8,7 @@
 """
 
 from . import *
+from telethon.utils import get_display_name
 
 
 @ultroid_cmd(pattern="totalmsgs ?(.*)")
@@ -23,6 +22,4 @@ async def _(e):
         user = "me"
     a = await e.client.get_messages(e.chat_id, 0, from_user=user)
     user = await e.client.get_entity(user)
-    await eor(e, f"Total msgs of `{user.first_name}` here = {a.total}")
-
-
+    await eor(e, f"total pesan dari `{get_display_name(user)}` disini = {a.total}")
